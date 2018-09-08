14a72e4531685dec0a8a21c0998564c0664d345a
annemariecabs <caballeroa1@bishopireton.org>
2018-09-08 10:16:25 -0400

    Work on fork branch to expand the functionality of the main Java class

--- Labs/HelloWorld/src/HelloWorld.java
+++ Labs/HelloWorld/src/HelloWorld.java
@@ -12,14 +12,34 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 		
-		System.out.println("Please enter an integer to add");
+		System.out.println("Please enter an integer");
 		int num1 = keyboard.nextInt();
-		System.out.println("Please enter another integer to add to the first");
+		System.out.println("Please enter another integer");
 		int num2 = keyboard.nextInt();
 		
-		System.out.println("\n2 + 2 = 5 \nkidding, not 1984"
-				+ "\n" + num1 + " + " + num2 + " = " + (num1 + num2));
-
+		System.out.println("Please choose an operation: +, -, *, /, or %"
+				+ "\nDo not add any additional spaces.");
+		String op = keyboard.next();
+		
+		System.out.println(returnOperation(num1, num2, op));
+	
+	}
+	
+	public static String returnOperation(int one, int two, String operator) {
+		double result = 0;
+		
+		switch(operator) {
+			
+		case "+": result = one + two;
+		case "-": result = one - two;
+		case "*": result = one * two;
+		case "/": result = one / two;
+		case "%": result = one % two;
+		
+		}
+		
+		return "\n2 + 2 = 5 \nkidding, not 1984" + "\n" + one + " " + operator + " " + two + " = " + result;
+		
 	}
 	
 }
