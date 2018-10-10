ee1a1ed4ab451ca845cb95d48dde525d235db88b
annemariecabs <caballeroa1@bishopireton.org>
2018-10-10 01:10:14 -0400

    still trying to fix javadocs

--- Javadocs/type-search-index.js
+++ Javadocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"p":"<Unnamed>","l":"Card"},{"p":"<Unnamed>","l":"CardComparator"},{"p":"<Unnamed>","l":"Deck"}]
+typeSearchIndex = [{"p":"<Unnamed>","l":"Card"},{"p":"<Unnamed>","l":"CardComparator"},{"p":"<Unnamed>","l":"Deck"},{"p":"<Unnamed>","l":"Test"}]
