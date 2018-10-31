d12b95c6ffd05d62e6e4eb7b0ae022672daafcf5
annemariecabs <caballeroa1@bishopireton.org>
2018-10-31 08:45:42 -0400

    final javadocs

--- Assignments/Sorting Cards/src/CardComparator.java
+++ Assignments/Sorting Cards/src/CardComparator.java
@@ -19,7 +19,7 @@ public class CardComparator implements Comparator<Card> {
 	/**
 	 * A boolean that keeps track of whether the Cards should be sorted in ascending
 	 * or descending order. If true, Cards should be sorted in ascending order.
-	 * If false, Cards should be sorted in ascending order.
+	 * If false, Cards should be sorted in descending order.
 	 */
 	private boolean ascend;
 	
